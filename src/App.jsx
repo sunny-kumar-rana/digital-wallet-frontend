a5cc2d500c44b87c1d3587768841ca75ae2626f4
Sunny <sunnysinghhrana@gmail.com>
2026-05-20 23:58:18 +0530

    -setup app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+
+function LoginPage() {
+  return <h1>Login Page</h1>
+}
+
+function RegisterPage() {
+  return <h1>Register Page</h1>
+}
+
+function DashboardPage() {
+  return <h1>Dashboard</h1>
+}
+
 function App() {
   return (
-    <div className="text-5xl font-bold text-center mt-20">
-      Digital Wallet Frontend
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
   )
 }
 

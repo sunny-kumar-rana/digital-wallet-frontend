50be6d47c86246db07d66272090eb597ff3d4896
Sunny <sunnysinghhrana@gmail.com>
2026-05-22 21:30:33 +0530

    -add register ui

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,58 @@
+import { Link } from "react-router-dom"
+
 function RegisterPage() {
+
     return (
-        <h1>Register Page</h1>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+
+            <div className="bg-white p-8 rounded-2xl shadow-lg w-[400px]">
+
+                <h1 className="text-3xl font-bold text-center mb-6">
+                    Create Account
+                </h1>
+
+                <form className="flex flex-col gap-4">
+
+                    <input
+                        type="text"
+                        placeholder="Enter name"
+                        className="border p-3 rounded-lg outline-none"
+                    />
+
+                    <input
+                        type="email"
+                        placeholder="Enter email"
+                        className="border p-3 rounded-lg outline-none"
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="Enter password"
+                        className="border p-3 rounded-lg outline-none"
+                    />
+
+                    <button
+                        type="submit"
+                        className="bg-black text-white p-3 rounded-lg hover:opacity-90"
+                    >
+                        Register
+                    </button>
+
+                </form>
+
+                <p className="mt-5 text-center">
+                    Already have an account?{" "}
+                    <Link
+                        to="/"
+                        className="text-blue-600 font-semibold"
+                    >
+                        Login
+                    </Link>
+                </p>
+
+            </div>
+
+        </div>
     )
 }
 

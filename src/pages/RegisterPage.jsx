55a666c4b44ec0b58e17c6ca55a77d4776aad0c2
Sunny <sunnysinghhrana@gmail.com>
2026-05-22 22:05:10 +0530

    -connect register api to axios -add state inside components -add submit function -connect form and inputs

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,59 +1,80 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
 
 function RegisterPage() {
+  const navigate = useNavigate();
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-            <div className="bg-white p-8 rounded-2xl shadow-lg w-[400px]">
+  const handleRegister = async (e) => {
+    e.preventDefault();
 
-                <h1 className="text-3xl font-bold text-center mb-6">
-                    Create Account
-                </h1>
+    try {
+      const response = await api.post("/register", {
+        name,
+        email,
+        password,
+      });
 
-                <form className="flex flex-col gap-4">
+      alert(response.data.message);
 
-                    <input
-                        type="text"
-                        placeholder="Enter name"
-                        className="border p-3 rounded-lg outline-none"
-                    />
+      navigate("/");
+    } catch (error) {
+      alert(error.response.data.error);
+    }
+  };
 
-                    <input
-                        type="email"
-                        placeholder="Enter email"
-                        className="border p-3 rounded-lg outline-none"
-                    />
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-lg w-[400px]">
+        <h1 className="text-3xl font-bold text-center mb-6">Create Account</h1>
 
-                    <input
-                        type="password"
-                        placeholder="Enter password"
-                        className="border p-3 rounded-lg outline-none"
-                    />
+        <form onSubmit={handleRegister} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border p-3 rounded-lg outline-none"
+          />
 
-                    <button
-                        type="submit"
-                        className="bg-black text-white p-3 rounded-lg hover:opacity-90"
-                    >
-                        Register
-                    </button>
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border p-3 rounded-lg outline-none"
+          />
 
-                </form>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border p-3 rounded-lg outline-none"
+          />
 
-                <p className="mt-5 text-center">
-                    Already have an account?{" "}
-                    <Link
-                        to="/"
-                        className="text-blue-600 font-semibold"
-                    >
-                        Login
-                    </Link>
-                </p>
+          <button
+            type="submit"
+            className="bg-black text-white p-3 rounded-lg hover:opacity-90"
+          >
+            Register
+          </button>
+        </form>
 
-            </div>
-
-        </div>
-    )
+        <p className="mt-5 text-center">
+          Already have an account?{" "}
+          <Link to="/" className="text-blue-600 font-semibold">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;

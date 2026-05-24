bc8bc2115fb2880d72b20b0d4e3e5c9ea0125369
Sunny <sunnysinghhrana@gmail.com>
2026-05-24 23:02:16 +0530

    -add dashboard block if not logged in -add logout button

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 function DashboardPage() {
   const [balance, setBalance] = useState(0);
+  const navigate = useNavigate();
 
   const userId = localStorage.getItem("userId");
   const name = localStorage.getItem("name");
 
   useEffect(() => {
+    if (!userId) {
+      navigate("/");
+      return;
+    }
+
     fetchBalance();
   }, []);
 
@@ -25,6 +32,16 @@ function DashboardPage() {
     <div className="min-h-screen bg-gray-100 p-10">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold mb-8">Welcome, {name}</h1>
+        <button
+          onClick={() => {
+            localStorage.clear();
+
+            navigate("/");
+          }}
+          className="bg-black text-white px-5 py-2 rounded-lg mb-8"
+        >
+          Logout
+        </button>
 
         <div className="bg-white p-8 rounded-2xl shadow-lg">
           <h2 className="text-2xl font-semibold mb-4">Wallet Balance</h2>
